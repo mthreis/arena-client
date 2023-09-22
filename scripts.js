38af82cba2f8eed1043c520649587b837e15fcc4
imokiswear <oreismatheus@gmail.com>
2023-09-22 10:57:56 -0300

    Standalone script for the page

---
+++ scripts.js
@@ -0,0 +1,47 @@
+
+
+function aa() {
+
+    items = [
+        {
+            name: "Matheus",
+            age: 28,
+            sport: "football"
+        },
+        {
+            name: "William",
+            age: 34,
+            sport: "basketball"
+        },
+    ]
+
+    const users = document.getElementById("users");
+    users.innerHTML="";
+
+    for(var i of items) {
+        users.innerHTML += 
+        `
+        <div class="userInfo"><span>${ i.name }</span> is ${ i.age } years old and likes ${ i.sport }.</div>
+        `;
+    }
+}
+
+async function submit() {
+    const res = await fetch("http://localhost:4005/users");
+
+    res.json().then(u => {
+        
+        console.log(u);
+
+        users.innerHTML="";
+
+        for(var i of u) {
+            users.innerHTML += 
+            `
+            <div class="userInfo"><span>${ i.firstName }</span> (${ i.age }${ i.sex }).</div>
+            `;
+        }
+    });
+
+    
+}
